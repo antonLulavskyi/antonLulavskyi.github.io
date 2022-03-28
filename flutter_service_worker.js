0da21cf58c755db7dc1e9619639b9289064fff96
Anton <42005312+antonLulavskyi@users.noreply.github.com>
2022-03-28 12:52:29 +0200

    Responsive design - bug fixes.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5cc9695ee3e8c1f86234c524152ea8f8",
-"index.html": "c64a53374524a71b582b60186e23c648",
-"/": "c64a53374524a71b582b60186e23c648",
-"main.dart.js": "7023832c72da9ba27008b1591ed21215",
+"index.html": "f4d0ba4eb2cca34f99cadfe85f51b9a7",
+"/": "f4d0ba4eb2cca34f99cadfe85f51b9a7",
+"main.dart.js": "1307674d3df3064bcc243cef6da446e0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
