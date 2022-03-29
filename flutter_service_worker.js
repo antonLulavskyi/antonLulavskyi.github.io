c6b7cc45d6372a9af10506ea6c0c601bd47da7f1
Anton <42005312+antonLulavskyi@users.noreply.github.com>
2022-03-29 16:02:46 +0200

    v1 resubmit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5cc9695ee3e8c1f86234c524152ea8f8",
-"index.html": "0bc4184556193b829293481b63a84282",
-"/": "0bc4184556193b829293481b63a84282",
-"main.dart.js": "a1de718276ab1764997cdb2baa496739",
+"index.html": "45c3f03a96786543e498e9b8290e4ce2",
+"/": "45c3f03a96786543e498e9b8290e4ce2",
+"main.dart.js": "b78c5d91beb1266b259c54c1b42d0ec4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
